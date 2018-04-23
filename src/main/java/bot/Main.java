b8f405c036feb1a44be369e2779efd1b28836c41
Giaco <giacomofm@outlook.it>
2018-04-23 19:20:50 +0200

    Aggiornamento per deploy su heroku

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -15,17 +15,23 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
  */
 public class Main {
 
-	public static void main(String[] args) {
+	private static final String externalUrl = "https://gfm-telegram-bot.herokuapp.com:80";
+	private static final String internalUrl = "https://127.0.0.1:" + (System.getenv("PORT") != null ? System.getenv("PORT") : "5000");
+
+	public static void main(final String[] args) {
+		System.out.println("URL interno: " + internalUrl);
+
 		ApiContextInitializer.init();
-		TelegramBotsApi botsApi = new TelegramBotsApi();
 		try {
+			final TelegramBotsApi botsApi = new TelegramBotsApi(externalUrl, internalUrl);
 			botsApi.registerBot(new Yoer());
 			System.out.println("Yoer inizializzato");
 			botsApi.registerBot(new JekOrologio());
 			System.out.println("JekOrologio inizializzato");
-		} catch (TelegramApiException e) {
+		} catch (final TelegramApiException e) {
 			e.printStackTrace();
 		}
+
 		System.out.println("Bot partiti");
 	}
 
